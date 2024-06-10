8ad8e49d26d3bb186e67fb50333968142f49c174
Conde <157609877+fvconde@users.noreply.github.com>
2024-06-10 15:03:25 -0300

    classes atualizadas

---
+++ aplicacao-officeeye/src/main/java/org/example/Funcionario.java
@@ -0,0 +1,73 @@
+package org.example;
+
+public class Funcionario {
+    private Integer idFuncionario;
+    private String nome;
+    private String email;
+    private String senha;
+    private String cpf;
+    private Integer fkEmpresa;
+
+
+    public Integer getIdFuncionario() {
+        return idFuncionario;
+    }
+
+    public void setIdFuncionario(Integer idFuncionario) {
+        this.idFuncionario = idFuncionario;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public Integer getFkEmpresa() {
+        return fkEmpresa;
+    }
+
+    public void setFkEmpresa(Integer fkEmpresa) {
+        this.fkEmpresa = fkEmpresa;
+    }
+
+    @Override
+    public String toString() {
+        return "Funcionario{" +
+                "idFuncionario=" + idFuncionario +
+                ", nome='" + nome + '\'' +
+                ", email='" + email + '\'' +
+                ", senha='" + senha + '\'' +
+                ", cpf='" + cpf + '\'' +
+                ", fkEmpresa=" + fkEmpresa +
+                '}';
+    }
+}
+
